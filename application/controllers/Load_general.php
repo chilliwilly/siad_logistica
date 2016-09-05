7eaa79716126bfd9072cae1f36085ceb0fb67e88
chilliwilly <w.contreras.f@gmail.com>
2016-09-05 11:13:55 -0300

    * cambios menores al controlador de carga general (se cambiano nombres de los modelos)

--- application/controllers/Load_general.php
+++ application/controllers/Load_general.php
@@ -3,12 +3,12 @@ date_default_timezone_set('Chile/Continental');
 class Load_general extends CI_Controller {
 	function __construct(){
 		parent::__construct();
-		$this->load->model('General_logistica_model');
-		$this->load->model('Seguridad_model');
+		$this->load->model('Logistica_general_model');
+		//$this->load->model('Seguridad_model');
 	}
 
   public function getNombreAliado(){
-    $list_aliado = $this->General_logistica_model->getStockListaAliado();
+    $list_aliado = $this->Logistica_general_model->getStockListaAliado();
     return json_encode($list_aliado);
   }
 }
